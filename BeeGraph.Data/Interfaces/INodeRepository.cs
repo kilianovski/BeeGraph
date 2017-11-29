db6ff57eb61c9f2a3523ac34f55bca45f2689fe9
mitutee <mitutee@gmail.com>
2017-11-29 22:04:26 +0200

    Implement first test for NodeRepository

---
+++ BeeGraph.Data/Interfaces/INodeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using BeeGraph.Domain;
+
+namespace BeeGraph.Data.Interfaces
+{
+    public interface INodeRepository
+    {
+        IEnumerable<Node> GetAll();
+        int CreateNode(string body);
+    }
+}
---
+++ BeeGraph.Tests/Integration/Repositories/NodeSpRepository.Tests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using BeeGraph.Data.Interfaces;
+using FsCheck;
+using FsCheck.Xunit;
+using Xunit;
+
+namespace BeeGraph.Tests.Integration
+{
+    public class NodeSpRepositoryTests
+    {
+        private readonly INodeRepository _nodeRepository;
+
+        [Property]
+        public bool ContainsNewlyCreatedEntity(string body)
+        {
+            if (string.IsNullOrEmpty(body)) body = "1"; // TODO
+
+            var entityId = _nodeRepository.CreateNode(body);
+            var allNodes = _nodeRepository.GetAll();
+
+            return allNodes.Any(n => n.Id == entityId);
+
+        }
+    }
+}
