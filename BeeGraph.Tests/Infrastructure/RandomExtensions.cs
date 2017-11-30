c2c30c2fadecbf760f39b380d5843a7d7ea05584
mitutee <mitutee@gmail.com>
2017-12-01 00:56:09 +0200

    Create red test for NodeRelationSpRepository

---
+++ BeeGraph.Tests/Infrastructure/RandomExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BeeGraph.Tests.Infrastructure
+{
+    public static class RandomExtensions
+    {
+        public static T PickRandomElem<T>(this IList<T> list)
+        {
+            var r = new System.Random();
+            int randomI = r.Next(0, list.Count - 1);
+            return list[randomI];
+        }
+    }
+}
---
+++ BeeGraph.Tests/Integration/Repositories/NodeRelationSpRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BeeGraph.Data;
+using BeeGraph.Data.Entities;
+using BeeGraph.Tests.Infrastructure;
+using FsCheck;
+using FsCheck.Xunit;
+
+namespace BeeGraph.Tests.Integration
+{
+    public class NodeRelationSpRepositoryTests
+    {
+        private readonly INodeRelationRepository _relationRepository;
+        private readonly IEdgeRepository _edgeRepository;
+        private readonly INodeRepository _nodeRepository;
+
+        public NodeRelationSpRepositoryTests()
+        {
+            _relationRepository = IoC.Container.GetInstance<INodeRelationRepository>();
+            _edgeRepository = IoC.Container.GetInstance<IEdgeRepository>();
+            _nodeRepository = IoC.Container.GetInstance<INodeRepository>();
+        }
+
+        [Property]
+        public bool ContainsNodeAfterAddition()
+        {
+            // arrange
+            var allNodes = _nodeRepository.GetAll().ToList();
+            var allEdges = _edgeRepository.GetAll().ToList();
+
+            EdgeEntity edge = allEdges.PickRandomElem();
+
+            NodeEntity fromNode = allNodes.PickRandomElem();
+            NodeEntity toNode = allNodes.PickRandomElem();
+
+            // act
+            int newRelationId = _relationRepository.AddRelation(edge.Id, fromNode.Id, toNode.Id);
+            var allRelations = _relationRepository.GetAll();
+
+            // assert
+            return allRelations.Any(r => r.Id == newRelationId);
+        }
+    }
+}
