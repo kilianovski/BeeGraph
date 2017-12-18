9bc75543fe2c5253ed95ff39948d5f0374118236
mitutee <mitutee@gmail.com>
2017-12-18 22:28:40 +0200

    Switch from WinForms to WPF app for editor

---
+++ Editor/BeeGraph.Editor/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BeeGraph.Data;
+
+namespace BeeGraph.Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var edgeRepository = IoC.IoC.Container.GetInstance<IEdgeRepository>();
+            var nodeRepository = IoC.IoC.Container.GetInstance<INodeRepository>();
+
+            DataContext = new RelationViewModel()
+            {
+                Nodes = nodeRepository.GetAll(),
+                Edges = edgeRepository.GetAll()
+            };
+        }
+    }
+}
---
+++ Editor/BeeGraph.Editor/RelationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using BeeGraph.Core.Domain;
+using BeeGraph.Data.Entities;
+
+namespace BeeGraph.Editor
+{
+    class RelationViewModel
+    {
+        public Node FromNode { get; set; }
+        public Node ToNode { get; set; }
+        public Edge Edge { get; set; }
+
+        public IEnumerable<NodeEntity> Nodes { get; set; }
+        public IEnumerable<EdgeEntity> Edges { get; set; }
+    }
+}
